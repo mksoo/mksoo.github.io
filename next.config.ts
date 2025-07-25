5176f6d0b982838600686b9473207dd76bb845ac
mksoo <mgs3067@naver.com>
2025-07-25 15:02:37 +0900

    init

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  basePath: isGithubPages ? "/mksoo-portfolio" : "",
+  assetPrefix: isGithubPages ? "/mksoo-portfolio" : "",
+  trailingSlash: true, // export시 필요하다고 함.
+  output: "export",
+  images: {
+    unoptimized: true, // ← 이 줄 추가!
+  },
 };
 
 export default nextConfig;
